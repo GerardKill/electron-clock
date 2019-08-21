07d6dd7bb8963f57c4e9e04ac4e894c823d3dfcf
Gerard Kill <vladmaydanskiy@gmail.com>
2019-08-21 21:08:36 +0300

    switch replaced with an array(2)

--- index.js
+++ index.js
@@ -24,58 +24,8 @@ function DateNow() {
 	let month = date__now.getMonth();
 	let year = date__now.getFullYear();
 	let w__day = date__now.getDay();
-	let text__month;
-	let text__w__day;
-	switch(month) {
-		case 0:
-		text__month = "January";
-		break;
-
-		case 1:
-		text__month = "February";
-		break;
-
-		case 2:
-		text__month = "March";
-		break;
-
-		case 3:
-		text__month = "April";
-		break;
-
-		case 4:
-		text__month = "May";
-		break;
-
-		case 5:
-		text__month = "June";
-		break;
-
-		case 6:
-		text__month = "July";
-		break;
-
-		case 7:
-		text__month = "August";
-		break;
-
-		case 8:
-		text__month = "September";
-		break;
-
-		case 9:
-		text__month = "October";
-		break;
-
-		case 10:
-		text__month = "November";
-		break;
-
-		case 11:
-		text__month = "December";
-		break;
-	}
+	let text__month = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
 	let text__w__day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-	document.getElementById('data').innerHTML = text__w__day[w__day] + ", " + text__month + " " + m__day + ", " + year;
+	document.getElementById('data').innerHTML = text__w__day[w__day] + ", " + text__month[month] + " " + m__day + ", " + year;
 }
